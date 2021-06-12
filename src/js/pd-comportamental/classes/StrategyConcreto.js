889b9ef65e5e06a4d1c0ef8d6659900b3ec30010
JG <diasusb@hotmail.com>
2021-06-12 02:58:59 -0300

    Add: Atualizar layout no container de texto

--- src/js/pd-comportamental/classes/StrategyConcreto.js
+++ src/js/pd-comportamental/classes/StrategyConcreto.js
@@ -7,21 +7,22 @@ export class StrategyConcreto {
     let resultName = this.pares.find((elemento) => {
       return elemento.par === `${name}`;
     });
+    
+    if (resultName.par === 'BRL'){
+      return `
+        (${name}) - Valor: ${valor}
+
+        A sigla BRL representa a moeda brasileira Real, que é a moeda corrente oficial da República Federativa do Brasil. Após sucessivas trocas monetárias, o Brasil adotou o real em 1 de julho de 1994, que, aliado à drástica queda das taxas de inflação, constituiu uma moeda estável para o país.
+      `;
+    }
 
     if (resultName.par === 'BTC'){
       return `
-        (${name} V) - Valor Inserido: ${valor}
+        (${name}) - Valor: ${valor}
 
         A sigla BTC representa a criptomoeda Bitcoin, que é uma criptomoeda descentralizada, sendo um dinheiro eletrônico para transações ponto-a-ponto. O primeiro artigo descrevendo uma implementação do Bitcoin foi apresentado em 2007 na lista de discussão The Cryptography Mailing por um programador ou grupo de programadores sob o pseudônimo Satoshi Nakamoto
       `;
     }
 
-    if (resultName.par === 'BRL'){
-      return `
-        (${name} V) - Valor Inserido: ${valor}
-
-        A sigla BRL representa a moeda brasileira Real, que é a moeda corrente oficial da República Federativa do Brasil. Após sucessivas trocas monetárias, o Brasil adotou o real em 1 de julho de 1994, que, aliado à drástica queda das taxas de inflação, constituiu uma moeda estável para o país.
-      `;
-    }
   }
 }
